1a67c1e76161817ba10eaa566297b6b6c9d03f14
melissa-murphy <melissamurphy.ca@gmail.com>
2019-09-20 23:14:18 -0700

    switching to gatsby

---
+++ stampclub/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <p>hello world</p>
+    </Layout>
+  )
+}
+export default IndexPage
