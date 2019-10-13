13fa03b715ced73c4eb9d8360c3fa586e09277d0
melissa-murphy <melissamurphy.ca@gmail.com>
2019-10-13 16:18:01 -0700

    update dealer, UX tweaks

--- parcel/src/index.js
+++ parcel/src/index.js
@@ -1,3 +1,15 @@
-import 'jquery'
-import 'popper.js'
-import 'bootstrap'
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+// const $ = jQuery;
+// $(document).ready(function() {
+//   const s = $('.nav-img');
+//   const pos = s.position();
+//   $(window).scroll(function() {
+//     const windowspos = $(window).scrollTop();
+//     (windowspos >= pos.top) & (windowspos <= 150)
+//       ? s.addClass('.scroll-shrink')
+//       : s.removeClass('.scroll-shrink');
+//   });
+// });
