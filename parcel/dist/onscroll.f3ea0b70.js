72caf66ca7a88dc3a00f1a8215d2a3a093208fc9
melissa-murphy <melissamurphy.ca@gmail.com>
2019-10-13 22:08:31 -0700

    new nav, shrink on scroll

--- parcel/dist/onscroll.f3ea0b70.js
+++ parcel/dist/onscroll.f3ea0b70.js
@@ -117,79 +117,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"assets/scss/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./..\\img\\fall-leaves.jpg":[["fall-leaves.498e173e.jpg","assets/img/fall-leaves.jpg"],"assets/img/fall-leaves.jpg"],"_css_loader":"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"scripts/onscroll.js":[function(require,module,exports) {
+$(document).ready(function () {
+  var s = $('.mainNavbar');
+  var pos = s.position();
+  $(window).scroll(function () {
+    var windowpos = $(window).scrollTop();
+    windowpos >= pos.top & windowpos <= 150 ? s.addClass('.scroll-shrink') : s.removeClass('.scroll-shrink');
+  });
+});
+},{}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59770" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62169" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -392,5 +329,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.90218242.js.map
+},{}]},{},["../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scripts/onscroll.js"], null)
+//# sourceMappingURL=/onscroll.f3ea0b70.js.map
--- parcel/src/index.js
+++ parcel/src/index.js
@@ -1,15 +1,3 @@
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
-
-// const $ = jQuery;
-// $(document).ready(function() {
-//   const s = $('.nav-img');
-//   const pos = s.position();
-//   $(window).scroll(function() {
-//     const windowspos = $(window).scrollTop();
-//     (windowspos >= pos.top) & (windowspos <= 150)
-//       ? s.addClass('.scroll-shrink')
-//       : s.removeClass('.scroll-shrink');
-//   });
-// });
